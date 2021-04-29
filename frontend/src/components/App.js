cb6351f7b9f3c863d23e3c849a900616a4e285a5
Andrew Lloyd <andrew.lloyd01@googlemail.com>
2021-04-29 14:34:49 -0700

    create frontend connected to rails backend and graphQL

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,9 +1,27 @@
-function App() {
-  return (
-    <div className="App">
-      <h1>Hello</h1>
-    </div>
-  )
+import React from 'react'
+import User from './User'
+import Users from './Users'
+
+class App extends React.Component {
+  state = {
+    selectedUser: null,
+  }
+
+  selectUser = (user) => {
+    this.setState({ selectedUser: user })
+  }
+
+  render() {
+    return (
+      <div className="container mx-auto px-4">
+        {this.state.selectedUser ? (
+          <User user={this.state.selectedUser} selectUser={this.selectUser} />
+        ) : (
+          <Users selectUser={this.selectUser} />
+        )}
+      </div>
+    )
+  }
 }
 
 export default App
